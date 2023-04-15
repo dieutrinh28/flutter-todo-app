bd35d3deaf9d2b548b9f993e087d2700c161afef
dieutrinh28 <trinhdo.work@gmail.com>
2023-04-15 17:24:42 +0700

    update todo

--- lib/screens/todo_list.dart
+++ lib/screens/todo_list.dart
@@ -9,53 +9,86 @@ class TodoList extends StatefulWidget {
 }
 
 class TodoListState extends State<TodoList> {
-
   List todoList = [];
   String newTodoTitle = "";
-  Stream todoListStream = FirebaseFirestore.instance.collection('todolist').snapshots();
-
+  Stream todoListStream =
+      FirebaseFirestore.instance.collection('todolist').snapshots();
 
   Future<void> createTodo() async {
     try {
-      await FirebaseFirestore.instance.collection('todolist').add(
-        {
-          'title': newTodoTitle,
-        }
-      );
+      await FirebaseFirestore.instance.collection('todolist').add({
+        'title': newTodoTitle,
+      });
+    } catch (e) {
+      print(e);
     }
-    catch (e) {
+  }
+
+  Future<void> updateTodo(String id, String title) async {
+    try {
+      await FirebaseFirestore.instance.collection('todolist').doc(id).update({
+        'title': title,
+      });
+    } catch (e) {
       print(e);
     }
   }
 
   void onAddTodoClick() {
     showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8),
-            ),
-            title: Text('New todo'),
-            content: TextField(
-              onChanged: (String value) {
-                newTodoTitle = value;
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          title: Text('New todo'),
+          content: TextField(
+            onChanged: (String value) {
+              newTodoTitle = value;
+            },
+          ),
+          actions: <Widget>[
+            ElevatedButton(
+              onPressed: () async {
+                await createTodo();
+                setState(() {
+                  todoList.add(newTodoTitle);
+                });
+                Navigator.of(context).pop();
               },
+              child: Text('Add'),
             ),
-            actions: <Widget>[
-              ElevatedButton(
-                onPressed: () async {
-                  await createTodo();
-                  setState(() {
-                    todoList.add(newTodoTitle);
-                  });
-                  Navigator.of(context).pop();
-                },
-                child: Text('Add'),
-              ),
-            ],
-          );
-        });
+          ],
+        );
+      },
+    );
+  }
+
+  void editTodo(BuildContext context, String id, String title) {
+    showDialog(context: context, builder: (BuildContext context) {
+      return AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        title: Text('Edit todo'),
+        content: TextField(
+          onChanged: (String value) {
+            newTodoTitle = value;
+          },
+          controller: TextEditingController(text: title),
+        ),
+        actions: <Widget>[
+          ElevatedButton(
+            onPressed: () async {
+              await updateTodo(id, newTodoTitle);
+              Navigator.of(context).pop();
+            },
+            child: Text('Save'),
+          ),
+        ],
+      );
+    });
   }
 
   @override
@@ -72,15 +105,14 @@ class TodoListState extends State<TodoList> {
         body: StreamBuilder(
           stream: todoListStream,
           builder: (context, snapshot) {
-
             List<DocumentSnapshot> documents = snapshot.data?.docs;
 
             return ListView.builder(
               itemCount: snapshot.data?.docs.length,
               padding: const EdgeInsets.all(12),
               itemBuilder: (BuildContext context, int index) {
-
-                String todoTitle = (documents[index].data() as Map<String, dynamic>)['title'] as String;
+                String todoTitle = (documents[index].data()
+                    as Map<String, dynamic>)['title'] as String;
                 return Dismissible(
                   key: Key(todoTitle),
                   child: Card(
@@ -90,13 +122,24 @@ class TodoListState extends State<TodoList> {
                     margin: EdgeInsets.all(8),
                     child: ListTile(
                       title: Text(todoTitle),
-                      trailing: IconButton(
+                      /*trailing: IconButton(
                         icon: Icon(Icons.delete),
                         color: Colors.red,
                         onPressed: () {
                           setState(() {
                             documents[index].reference.delete();
                           });
+                        },
+                      ),*/
+
+                      trailing: IconButton(
+                        icon: Icon(Icons.edit),
+                        onPressed: () {
+                          editTodo(
+                            context,
+                            snapshot.data?.docs[index].id,
+                            snapshot.data?.docs[index]['title'],
+                          );
                         },
                       ),
                     ),
@@ -106,7 +149,7 @@ class TodoListState extends State<TodoList> {
             );
           },
         )
-      /*body: ListView.builder(
+        /*body: ListView.builder(
         itemCount: todoList.length,
         padding: const EdgeInsets.all(12),
         itemBuilder: (BuildContext context, int index) {
@@ -133,6 +176,6 @@ class TodoListState extends State<TodoList> {
           );
         },
       ),*/
-    );
+        );
   }
 }
